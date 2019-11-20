75ce82a9e7229267d6a672a2210a7e0e302d6641
min1378 <qwes123@naver.com>
2019-11-20 15:52:57 +0900

    Database complete

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=20)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    audience = models.IntegerField()
+    poster_url = models.CharField(max_length=140)
+    description = models.TextField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='movies')
+    liked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_movies')
+
+class Review(models.Model):
+    content = models.CharField(max_length=50)
+    score = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews_users')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews_movies')
